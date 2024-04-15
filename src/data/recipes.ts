a52f576821af17961c632f240025de23caf8f344
yuiyui910 <yui1423@icloud.com>
2024-04-15 21:41:43 +0900

    TOPページの表示

---
+++ src/data/recipes.ts
@@ -0,0 +1,13 @@
+import { recipe } from "types/recipe";
+
+export const recipes: recipe[] = [
+  {
+    //IDはランダム文字列にしたい
+    id: "0000000001",
+    name: "オムライス",
+  },
+  {
+    id: "0000000002",
+    name: "カレーライス",
+  },
+];
---
+++ src/features/top/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Top = () => {
+  return <div>Topページです</div>;
+};
